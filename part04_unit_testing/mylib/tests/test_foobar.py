f440fd4baf2709a8702a151db06a5df2218663f4
Ezio <assassinste@gmail.com>
2019-12-04 10:10:29 +0800

    Adding some tests using pytest (skip if ..)

--- part04_unit_testing/mylib/tests/test_foobar.py
+++ part04_unit_testing/mylib/tests/test_foobar.py
@@ -1,10 +1,16 @@
+import pytest
+
+
 def test_true() -> None:
     assert True
 
+
+@pytest.mark.skip("Do not run this")
 def test_false() -> None:
     assert False
 
+
 def test_key() -> None:
-    a = ['a', 'b']
-    b = ['b']
+    a = ["a", "b"]
+    b = ["b"]
     assert a == b
